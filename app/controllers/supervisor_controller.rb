be95fea1018ff80063c63b4374eb2ace327b1e14
maliha <malihatasnim753@gmail.com>
2025-01-12 13:59:26 +0800

    added header to financials dashboard

--- app/controllers/supervisor_controller.rb
+++ app/controllers/supervisor_controller.rb
@@ -4,6 +4,13 @@ class SupervisorController < ApplicationController
 
   def dashboard
     flash[:success] = "Welcome supervisor."
+
+    # Fetch all borrowings along with related data
+    @borrowings = Borrowing.includes(:equipment, :club).all
+
+    # Fetch PICs for clubs from user_data
+    @pic_data = UserData.where(is_supervisor: false).group_by(&:club_id)
+
     render "auth/supervisor/dashboard"
   end
 end
